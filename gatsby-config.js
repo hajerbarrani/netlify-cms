83e9dff3df22671685953ce521c94477e98a7bf8
guest <Hajer.Barrani@ext.saint-gobain.com>
2018-10-30 18:06:56 +0100

    add changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,39 @@
 module.exports = {
-    plugins: [`gatsby-plugin-netlify-cms`],
-}
+    siteMetadata: {
+      title: 'Hajer test Gatsby + Netlify CMS Starter',
+    },
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      'gatsby-plugin-sass',
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: `${__dirname}/src/pages`,
+          name: 'pages',
+        },
+      },
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: `${__dirname}/src/assets`,
+          name: 'assets',
+        },
+      },
+      'gatsby-plugin-sharp',
+      'gatsby-transformer-sharp',
+      {
+        resolve: 'gatsby-transformer-remark',
+        options: {
+          plugins: [],
+        },
+      },
+      {
+        resolve: 'gatsby-plugin-netlify-cms',
+        options: {
+          modulePath: `${__dirname}/src/cms/cms.js`,
+        },
+      },
+      'gatsby-plugin-netlify', // make sure to keep it last in the array
+    ],
+  }
+  
